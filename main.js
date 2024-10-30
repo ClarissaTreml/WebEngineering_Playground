a3219361f230c34adbeb0c495175b064cbfe1822
claristrin <clarissa.treml@gmail.com>
2024-10-30 22:59:30 +0100

    - adapted to make main.js the only entrypoint - added to all functions the async/await and the arrow function syntax - proper renaming

--- main.js
+++ main.js
@@ -1,12 +1,8 @@
-import { getBearData } from './fetch.js';  // Correct
+import { getBearData } from './modules/fetchingData.js';
+import { addComment, toggleComments } from './modules/commentSection.js';
 
-// Function to fetch the image URLs based on the file names
-const baseUrl = "https://en.wikipedia.org/w/api.php";
-
-// Fetch and display the bear data
-document.addEventListener('DOMContentLoaded', () => {
-    getBearData();
+document.addEventListener('DOMContentLoaded', async () => {
+    toggleComments();
+    addComment();
+    await getBearData();
 });
-
-// Export these functions so they can be used in other modules
-export { baseUrl };
---
+++ modules/commentSection.js
@@ -0,0 +1,48 @@
+const toggleComments = () => {
+    const showHideBtn = document.querySelector('.show-hide');
+    const commentWrapper = document.querySelector('.comment-wrapper');
+    if (!showHideBtn || !commentWrapper) return;  // Check if elements exist
+
+    commentWrapper.style.display = 'none';
+
+    showHideBtn.onclick = () => {
+        const showHideText = showHideBtn.textContent;
+        if (showHideText === 'Show comments') {
+            showHideBtn.textContent = 'Hide comments';
+            commentWrapper.style.display = 'block';
+        } else {
+            showHideBtn.textContent = 'Show comments';
+            commentWrapper.style.display = 'none';
+        }
+    };
+};
+
+const addComment = () => {
+    const form = document.querySelector('.comment-form');
+    const nameField = document.querySelector('#name');
+    const commentField = document.querySelector('#comment');
+    const list = document.querySelector('.comment-container');
+    if (!form || !nameField || !commentField || !list) return;  // Check if elements exist
+
+    form.onsubmit = (e) => {
+        e.preventDefault();
+        const listItem = document.createElement('li');
+        const namePara = document.createElement('p');
+        const commentPara = document.createElement('p');
+        const nameValue = nameField.value;
+        const commentValue = commentField.value;
+
+        namePara.textContent = nameValue;
+        commentPara.textContent = commentValue;
+
+        list.appendChild(listItem);
+        listItem.appendChild(namePara);
+        listItem.appendChild(commentPara);
+
+        nameField.value = '';
+        commentField.value = '';
+    };
+};
+
+export {toggleComments, addComment}
+
